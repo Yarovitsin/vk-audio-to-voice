528b1a5b05e88359b92d71b94cbfa0bc697fb625
Vanya Iarovitzine <ivphoneac@gmail.com>
2023-04-17 04:10:33 +0300

    install script fix

--- install.sh
+++ install.sh
@@ -10,4 +10,4 @@ sudo ln -s $(pwd)/vk-audio-to-voice.sh /usr/local/bin/vk-audio-to-voice
 # Set up the Python script
 sudo ln -s $(pwd)/vk-audio-to-voice.py /usr/local/bin/vk-audio-to-voice.py
 
-echo "Installation complete. You can now run the script with 'script' or 'python3 script.py'."
+echo "Installation complete. You can now run the script with 'vk-audio-to-voice' or 'vk-audio-to-voice.py'."
